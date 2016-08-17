1af41b0d132147a9814064b59d916a6905518475
Sanjay Maharjan <sanmaharjan@deerwalk.com>
2016-08-17 18:16:21 +0545

    project initiation

---
+++ Angular2WithCore/src/Angular2WithCore/gulpfile.js
@@ -0,0 +1,29 @@
+﻿/// <binding />
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+var destPath = './wwwroot/libs/';
+
+// Delete the dist directory
+gulp.task('clean', function () {
+    return gulp.src(destPath)
+        .pipe(clean());
+});
+
+gulp.task("scriptsNStyles", () => {
+    gulp.src([
+            'core-js/client/**',
+            'systemjs/dist/system.src.js',
+            'reflect-metadata/**',
+            'rxjs/**',
+            'zone.js/dist/**',
+            '@angular/**',
+            'jquery/dist/jquery.*js',
+            'bootstrap/**'
+    ], {
+        cwd: "node_modules/**"
+    })
+        .pipe(gulp.dest("./wwwroot/node_modules"));
+});
+
+gulp.task('default', ['scriptsNStyles']);
